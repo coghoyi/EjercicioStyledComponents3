f571f2f69dc8196863ca35bab60b84b90735626a
coghoyi <davidhoyolopez@alumnostrazos.net>
2023-06-09 13:06:31 +0200

    ApuntesFormulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import { GlobalStyles } from "./styles/globalStyles";
+
 const App = () => {
-	return <h1>Núcleo de la aplicación</h1>;
+	const [name, setName] = useState('')
+	const [email, setEmail] = useState('')
+	return (
+		<>
+		<GlobalStyles/>
+		<h1>{name}</h1>
+		<h1>{email}</h1>
+		<form onSubmit={e => e.preventDefault()}>
+			<div>
+				<label>Name</label>
+				<input type="text" onChange={e => setName(e.target.value)}/>
+			</div>
+			<div>
+				<label>Email</label>
+				<input type="text" onChange={e => setEmail(e.target.value)}/>
+			</div>
+			<button>Enviar</button>
+		</form>
+		</>
+	)
 };
 
 export default App;
